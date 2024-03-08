def68223d4ff52d3c2cfb70a3428a31973f0f937
Diana Ramos <diana.sd.ramos@gmail.com>
2024-03-08 12:21:29 +0000

    signup and login pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { AuthProviderWrapper } from "./context/auth.context"; // <== IMPORT
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ 
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <AuthProviderWrapper>      {/*  <== ADD  */}
+        <App />
+      </AuthProviderWrapper>     {/*  <== ADD  */}
+    </Router>
+  </React.StrictMode>
+);
