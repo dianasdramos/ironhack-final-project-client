51a8cead99140f17fd68dd8c7e63137da421d113
Diana Ramos <diana.sd.ramos@gmail.com>
2024-03-08 11:22:26 +0000

    context

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,8 +1,24 @@
-/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-// Initializing Context
+const API_URL = "http://localhost:5005";
+ 
 const AuthContext = React.createContext();
 
-const API_URL = "http://localhost:5005";
+function AuthProviderWrapper(props) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [user, setUser] = useState(null);
+    
+    /* 
+      Functions for handling the authentication status (isLoggedIn, isLoading, user)
+      will be added here later in the next step
+    */
+   
+    return (
+      <AuthContext.Provider value={{ isLoggedIn, isLoading, user }}>
+        {props.children}
+      </AuthContext.Provider>
+    )
+  }
+   
+  export { AuthProviderWrapper, AuthContext };
