23f9d2fbda97b39504c051f98e965e04ec8f7e67
Tomás Bastos <soutomasbastos@gmail.com>
2024-03-08 16:29:31 +0000

    routes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,87 @@
+/* REACT */
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+
+/* COMPONENTS */
 import Navbar from "./src/components/Navbar";
+import Footer from "./src/components/Footer";
+import IsAnon from "./src/components/IsAnon";
+import IsPrivate from "./src/components/IsPrivate";
+
+/* PAGES */
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
+import Profile from "./pages/Profile";
+
 import About from "./pages/About";
+import Homepage from "./pages/Homepage";
+import Error from "./pages/Error";
+
 import AddPhoto from "./pages/AddPhoto";
+import EditPhoto from "./pages/EditPhoto";
+import PhotoDetails from "./pages/PhotoDetails";
+
 import CameraDetails from "./pages/CameraDetails";
 import CameraList from "./pages/CameraList";
-import EditPhoto from "./pages/EditPhoto";
-import Error from "./pages/Error";
-import Homepage from "./pages/Homepage";
-import PhotoDetails from "./pages/PhotoDetails";
-import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
 
+      {/* ANON ROUTES */}
       <Routes>
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/signup"
+          element={
+            <IsAnon>
+              <SignupPage />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <IsAnon>
+              <LoginPage />
+            </IsAnon>
+          }
+        />
+        {/* PRIVATE ROUTES */}
+        <Route
+          path="/photo"
+          element={
+            <IsPrivate>
+              <AddPhoto />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/photos/edit/:photoId"
+          element={
+            <IsPrivate>
+              <EditPhoto />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <IsPrivate>
+              <Profile />
+            </IsPrivate>
+          }
+        />
+        {/* PUBLIC ROUTES */}
+        <Route path="/" element={<Homepage />} />
+        <Route path="/photos/:photoId" element={<PhotoDetails />} />
+        <Route path="/cameras" element={<CameraList />} />
+        <Route path="/cameras/:cameraId" element={<CameraDetails />} />{" "}
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+        {/* FOOTER */}
+        <Footer />
       </Routes>
     </div>
   );
