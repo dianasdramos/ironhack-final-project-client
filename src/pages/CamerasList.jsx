c3f45dd4fe75a5239cadb920c08487d16f77510c
Tomás Bastos <soutomasbastos@gmail.com>
2024-03-11 15:01:13 +0000

    cameras list

--- src/pages/CamerasList.jsx
+++ src/pages/CamerasList.jsx
@@ -1,12 +1,33 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-
-function CamerasList () {
-    return (
-        <div>
-            <h1>hello world</h1>
-        </div>
-    )
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+const API_URL = "http://localhost:5005/api";
+function CamerasList() {
+  const [cameras, setCameras] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/cameras`)
+      .then((response) => setCameras(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <div className="planet-container">
+      {cameras &&
+        cameras.map((camera) => {
+          return (
+            <section key={camera.id} className="cards ">
+              <Link to={`/cameras/${camera.id}`}>
+                <div className="img-container">
+                  <img src={camera.image} />
+                </div>
+                <div className="name-container">
+                  <h1>{camera.name}</h1>
+                </div>
+              </Link>
+            </section>
+          );
+        })}
+    </div>
+  );
 }
-
 export default CamerasList;
