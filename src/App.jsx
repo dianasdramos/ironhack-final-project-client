e4f2438d7a3cfdf7f846aa39f1a9f7a7ddf464c7
Diana Ramos <diana.sd.ramos@gmail.com>
2024-03-12 14:59:31 +0000

    user page updated

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ function App() {
           }
         />
         <Route
-          path="/profile"
+          path="/profile/:id"
           element={
             <IsPrivate>
               <Profile />
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,64 @@
-import { useContext } from "react";
+import { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import placeholderImage from "./../assets/profile_pic.jpg";
+
 import { AuthContext } from "../context/auth.context";
 
-function Profile() {
+const API_URL = "http://localhost:5005/api";
 
-  const {user} = useContext(AuthContext);
+function Profile() {
+  const [userProfile, setUserProfile] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { user } = useContext(AuthContext);
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  useEffect(() => {
+    const getUser = () => {
+      const storedToken = localStorage.getItem("authToken");
+
+      if (storedToken) {
+        axios
+        .get(
+          `${API_URL}/profile/${user._id}`,
+          { headers: { Authorization: `Bearer ${storedToken}` }}
+          )
+          .then((response) => {
+            setUserProfile(response.data);
+            setLoading(false);
+          })
+          .catch((error) => {
+            const errorDescription = error.response.data.message;
+            setErrorMessage(errorDescription);
+          });
+        }
+        else {
+          setErrorMessage("User not logged in");
+        }
+    };
+
+    getUser();
+  }, [user._id]);
+
+  if (errorMessage) return <div>{errorMessage}</div>;
+  
+  if (loading) return <div>Loading...</div>;
 
   return (
-    <section>
+    <div>
       <div>
-        <h4>{user.name}</h4>
-        <p>{user.email}</p>
+        {userProfile && (
+          <>
+            {/* <img className="w-32 h-32 rounded-full object-cover mb-4" src={student.image} alt="profile-photo" /> */}
+            <img
+            src={placeholderImage}
+            alt="profile-photo"
+          />            
+            <h1>{userProfile.name}</h1>
+          </>
+        )}
       </div>
-    </section>
+      
+    </div>
   );
 }
 
