530b1cbc0eeb2c5937f4c6a229f33bdd8f01d04d
Diana Ramos <diana.sd.ramos@gmail.com>
2024-03-14 20:34:15 +0000

    error and about pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import githubIcon from "./../assets/github.png"
 import linkedinIcon from "./../assets/linkedin.png"
 
@@ -5,27 +6,33 @@ function About() {
 
     return (
         <section className="text-black flex flex-col justify-center">
-            <div className="m-4">
-                <h4>Description?</h4>
+            <div className="mt-8 mx-20">
+                <h4 className="text-center">Welcome to our collaborative vintage photo gallery! Dive into a world of nostalgia with our curated collection of timeless images contributed by users worldwide. Join our community and share your own vintage treasures, preserving the beauty of the past for future generations to enjoy.</h4>
             </div>
-            <div className="flex flex-col m-4">
+            <div>
+                <p className="mt-8 mx-20">To learn more about the creators behind this project, visit our profiles:</p>
+            </div>
+            <div className="flex flex-col m-8">
                 <div>
-                    <p className="text-lg">Tomás Bastos</p>
+                    <p className="text-lg font-bold">Tomás Bastos</p>
                 </div>
-                <div className="flex justify-center">
-                    <a href="https://github.com/Tom4sB4stos" target="_blank"><img className="w-10 h-10" src={githubIcon} alt="github-icon" /></a>
-                    <a href="https://www.linkedin.com/in/tom%C3%A1s-bastos-3aa674136/" target="_blank"><img className="w-10 h-10" src={linkedinIcon} alt="linkedin-icon" /></a>
+                <div className="flex justify-center m-3">
+                    <a className="m-1" href="https://github.com/Tom4sB4stos" target="_blank"><img className="w-9 h-9" src={githubIcon} alt="github-icon" /></a>
+                    <a className="m-1" href="https://www.linkedin.com/in/tom%C3%A1s-bastos-3aa674136/" target="_blank"><img className="w-9 h-9" src={linkedinIcon} alt="linkedin-icon" /></a>
                 </div>
             </div>
             <div>
-                <div className="flex flex-col m-4">
+                <div className="flex flex-col">
                     <div>
-                        <p className="text-lg">Diana Ramos</p>
+                        <p className="text-lg font-bold">Diana Ramos</p>
                     </div>
                 </div>
-                <div className="flex justify-center">
-                    <a href="https://github.com/dianasdramos" target="_blank"><img className="w-10 h-10" src={githubIcon} alt="github-icon" /></a>
-                    <a href="https://www.linkedin.com/in/dianaramos/" target="_blank"><img className="w-10 h-10" src={linkedinIcon} alt="linkedin-icon" /></a>
+                <div className="flex justify-center m-3">
+                    <a className="m-1" href="https://github.com/dianasdramos" target="_blank"><img className="w-9 h-9" src={githubIcon} alt="github-icon" /></a>
+                    <a className="m-1" href="https://www.linkedin.com/in/dianaramos/" target="_blank"><img className="w-9 h-9" src={linkedinIcon} alt="linkedin-icon" /></a>
+                </div>
+                <div>
+                    <Link to="/"><button className="text-white hover:bg-gray-400 m-10">Back</button></Link>
                 </div>
             </div>
         </section>
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom"
+import error from "./../assets/error.jpg"
 
 function Error() {
     return (
-        <section>
-            <p>Oops... Page not found!</p>
-            <Link to="/"><button>Go Back Home</button></Link>
+        <section className="flex justify-center flex-col">
+            <div className="flex justify-center mt-8">
+                <img className="w-80 h-80" src={error} />
+            </div>
+            <div>
+                <p className="m-6 text-2xl">Oops... Page not found!</p>
+            </div>
+            <div>
+                <Link to="/"><button className="text-white hover:bg-gray-400 m-4">Go Back Home</button></Link>
+            </div>
         </section>
     )
 }
